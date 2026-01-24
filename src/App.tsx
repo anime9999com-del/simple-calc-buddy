634ef592a61b6ac005a48cb18e14474d09d9af07
anime9999com-del <anime9999.com@gmail.com>
2026-01-24 14:00:28 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/lib/auth";
+import Index from "./pages/Index";
+import Auth from "./pages/Auth";
+import FindListener from "./pages/FindListener";
+import Bookings from "./pages/Bookings";
+import AdminLogin from "./pages/AdminLogin";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/find-listener" element={<FindListener />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </AuthProvider>
+  </QueryClientProvider>
+);
+
+export default App;
